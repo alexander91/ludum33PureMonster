ba07ca4f4bb0f75e47208dd39263fbcf820b6640
alexander91 <alexander_gri@mail.ru>
2015-08-23 19:35:57 +0300

    better moving of enemies

--- PureMonster/Assets/Scripts/Enemy.cs
+++ PureMonster/Assets/Scripts/Enemy.cs
@@ -3,26 +3,32 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField]
+    bool active = true;
+
     Renderer renderer;
 
     class WandererControl
     {
-        public float speed = 0.01f;
-        public const float turnSpeed = 0.1f;
-        public const float maxGoingTime = 5f;
+        public WandererControl()
+        {
+
+        }
+
+        public float speed = 0.03f;
+        public const float maxGoingTime = 3f;
 
         Vector3 nextPosition;
         float startGoingTime;
 
-        public void TurnToPosition(Transform objectToTurn)
+        public void GoToPosition(Transform transform)
         {
-            objectToTurn.rotation *= (Quaternion.Lerp(
-                Quaternion.FromToRotation(objectToTurn.rotation * Vector3.right, nextPosition - objectToTurn.position), 
-                Quaternion.identity, turnSpeed));
+            transform.position += speed * (nextPosition - transform.position).normalized;
         }
 
         public void Iterate()
         {
+            //if ()
             if ((Time.time - startGoingTime) > maxGoingTime)
             {
                 SetNewTarget();
@@ -32,7 +38,9 @@ public class Enemy : MonoBehaviour {
 
         void SetNewTarget()
         {
-            nextPosition = RandomUtils.getRandomVector(45f);
+            nextPosition = RandomUtils.getRandomVector(185f);
+            Debug.Log(nextPosition);
+            nextPosition.z = 0f;
         }
     }
 
@@ -74,8 +82,9 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position += myWalker.speed * Vector3.right;
-        myWalker.TurnToPosition(transform);
+        if (!active) return;
+
+        myWalker.GoToPosition(transform);
         myWalker.Iterate();
 	
 	}
